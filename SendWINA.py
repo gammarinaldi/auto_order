f541176affb0b85f53e2d169ba723b600085441b
Gamma Rinaldi <gammarinaldi@yahoo.com>
2022-09-07 00:51:32 +0700

    Implement async create order

--- SendWINA.py
+++ SendWINA.py
@@ -1,16 +1,27 @@
+from pickle import FALSE
+import undetected_chromedriver as uc
+import csv
+import telegram
+import time
+import os
+import order
+import concurrent.futures
+import threading
+import json
+from dotenv import load_dotenv
+from concurrent.futures import ThreadPoolExecutor
+
 if __name__ == '__main__':
-    import undetected_chromedriver as uc
-    import csv
-    import telegram
-    import time
-    import os
-    import order
-    from dotenv import load_dotenv
-    from selenium.webdriver.common.by import By
-
-    print("Performing WINA...")
-
     try:
+        def get_user_data():
+            f = open('auto_order\data.json')
+            users = json.load(f)
+            f.close()
+            return users
+
+        USERS = get_user_data()
+        
+        # Check csv content
         def is_empty_csv(path):
             with open(path) as csvfile:
                 reader = csv.reader(csvfile)
@@ -19,66 +30,130 @@ if __name__ == '__main__':
                         return False
             return True
 
-        # Define object
-        class data():
+        # Define data order params
+        class data_order():
             def __init__(self, emiten, buy_price, take_profit, cut_loss):
                 self.emiten = emiten
                 self.buy_price = buy_price
                 self.take_profit = take_profit
                 self.cut_loss = cut_loss
 
+        # Get web driver
+        def get_driver():
+            threadLocal = threading.local()
+            driver = getattr(threadLocal, 'driver', None)
+            if driver is None:
+                options = uc.ChromeOptions()
+                options.headless=True
+                options.add_argument('--headless')
+                driver = uc.Chrome(options=options)
+                setattr(threadLocal, 'driver', driver)
+            return driver
+
+        def buy(user, list_order):
+            driver = get_driver()
+            print(driver)
+            order.login(user, driver)   
+
+            for obj in list_order:
+                order.create_buy_order(user, driver, obj.emiten, obj.buy_price)
+
+            order.logout(user, driver)
+            order.delete_cache(user, driver)
+
+        def sell(user, list_order):
+            driver = get_driver()
+            print(driver)
+            order.login(user, driver)   
+
+            for obj in list_order:
+                order.create_sell_order(user, driver, obj.emiten, obj.take_profit, obj.cut_loss)
+
+            order.logout(user, driver)
+            order.delete_cache(user, driver)
+
+        def async_buy(list_order):
+             with ThreadPoolExecutor(max_workers=10) as executor:
+                future_to_user = {executor.submit(buy, user, list_order): user for user in USERS}
+                for future in concurrent.futures.as_completed(future_to_user):
+                    user = future_to_user[future]
+                    try:
+                        future.result()
+                    except Exception as exc:
+                        print('%r generated an exception: %s' % (user["email"], exc))
+
+        def async_sell(list_order):
+            with ThreadPoolExecutor(max_workers=10) as executor:
+                future_to_user = {executor.submit(sell, user, list_order): user for user in USERS}
+                for future in concurrent.futures.as_completed(future_to_user):
+                    user = future_to_user[future]
+                    try:
+                        future.result()
+                    except Exception as exc:
+                        print('%r generated an exception: %s' % (user["email"], exc))
+
+        def send_signal(chat_ids, msg):
+            for chat_id in chat_ids:
+                bot.send_message(chat_id=chat_id, text=msg, parse_mode=telegram.ParseMode.MARKDOWN_V2)
+        
+        print("Performing WINA...\n")
+
+        # Load config env
         load_dotenv()
-    
+
         TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
         TELEGRAM_CHAT_IDS = [os.getenv('TELEGRAM_CHAT_ID_WINA'), os.getenv('TELEGRAM_CHAT_ID_SINYALA')]
         TELEGRAM_LOGGER_ID = os.getenv('TELEGRAM_LOGGER_ID')
         bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
 
-        list = []
-        path = r"C:\Users\Superadmin\Desktop\GamaTradingSystem\WINAReport.csv"
+        list_order = []
+        path = r"C:\Users\Gama\Downloads\GamaTradingSystem\WINAReport.csv"
 
         with open(path, "r") as file:
             csvreader = csv.reader(file)
             if is_empty_csv(path) == False:
                 next(csvreader, None)
 
-                # Init auto order
-                options = uc.ChromeOptions()
-                options.headless=True
-                options.add_argument('--headless')
-                driver = uc.Chrome(options=options)
-
-                order.delete_cache(driver)
-                order.login(driver)
-
                 for row in csvreader:
-                    emiten = row[0].replace(".JK", "")
+                    emiten = row[0]
+                    if row[0].find(".JK") != -1:
+                        emiten = emiten.replace(".JK", "")
                     signal_date = row[1].split(" ")[0]
-                    close = row[2]
-                    change = row[3]
-                    trx = row[4]
+                    # close = row[2]
+                    # change = row[3]
+                    # trx = row[4]
                     buy_price = row[5]
                     take_profit = row[6]
                     cut_loss = row[7]
                     
                     msg = "💌 Rekomendasi WINA \(" + signal_date + "\)\n\n*Buy $" + emiten + "\nBuy @" + buy_price + "\nTake Profit @" + take_profit + "\nCutloss @" + cut_loss + "*\n\n_Disclaimer ON\. DYOR\._"
 
-                    for chat_id in TELEGRAM_CHAT_IDS:
-                        bot.send_message(chat_id=chat_id, text=msg, parse_mode=telegram.ParseMode.MARKDOWN_V2)
+                    # Send signal to telegram
+                    # send_signal(TELEGRAM_CHAT_IDS, msg)
 
-                    # Input data for auto order
-                    list.append(data(emiten, buy_price, take_profit, cut_loss))
+                    # Input order parameters for auto order
+                    list_order.append(data_order(emiten, buy_price, take_profit, cut_loss))
 
-                # Send buy order to sekuritas
-                for obj in list:
-                    order.create_buy_order(driver, obj.emiten, obj.buy_price)
+                t1 = time.time()
 
-                print('Wait 1 hour to create auto order')
-                time.sleep(3600)
+                # Send async buy order to sekuritas
+                async_buy(list_order)
 
-                # Send auto sell order to sekuritas
-                for obj in list:
-                    order.create_auto_order(driver, obj.emiten, obj.take_profit, obj.cut_loss)
+                t2 = time.time()
+                diff = t2 -t1
+                print("Processing buy order takes: " + str(round(diff, 2)) + " secs.")
+
+                print('Wait 1 hour to create auto sell order')
+                time.sleep(5)
+
+                t1 = time.time()
+
+                # Send async auto sell order to sekuritas
+                async_sell(list_order)
+
+                t2 = time.time()
+                diff = t2 -t1
+                print("Processing buy order takes: " + str(round(diff, 2)) + " secs.")
             else: 
                 msg = "Sorry, no signal for today."
                 bot.send_message(chat_id=TELEGRAM_LOGGER_ID, text=msg)
--- order.py
+++ order.py
@@ -14,56 +14,65 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-EMAIL = os.getenv('EMAIL')
-PASSWORD = os.getenv('PASSWORD')
-PIN1 = os.getenv('PIN1')
-PIN2 = os.getenv('PIN2')
-PIN3 = os.getenv('PIN3')
-PIN4 = os.getenv('PIN4')
 LOGIN_URL = os.getenv('LOGIN_URL')
+LOGOUT_URL = os.getenv('LOGOUT_URL')
 HOMEPAGE_URL = os.getenv('HOMEPAGE_URL')
 
 # TODO: login data encryption
 
 DELAY = 10 # seconds
 
-def login(driver):
+def login(user, driver):
+    user_email = user["email"]
+    user_pass = user["password"]
+    user_pin = user["pin"]
+
     # Input login
     driver.get(LOGIN_URL + '/login')
 
     email = driver.find_element(By.XPATH, '//input[@name="email"]')
-    email.send_keys(EMAIL)
+    email.send_keys(user_email)
 
     password = driver.find_element(By.XPATH, '//input[@name="password"]')
-    password.send_keys(PASSWORD)
+    password.send_keys(user_pass)
     password.send_keys(Keys.RETURN)
 
-    print('Input login OK')
+    print(user_email + ": input login")
 
     # Input PIN
+    pins = []
+    for pin in user_pin:
+        pins.append(pin)
+
     try:
         WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.XPATH, '//input[@class="pincode-input-text"]')))
-        print("Input PIN OK")
+        print(user_email + ": input PIN")
 
         pin1 = driver.find_element(By.XPATH, '//input[@class="pincode-input-text"]')
-        pin1.send_keys(PIN1)
+        pin1.send_keys(pins[0])
 
         pin2 = driver.find_element(By.XPATH, '//input[@class="pincode-input-text"]/following::input')
-        pin2.send_keys(PIN2)
+        pin2.send_keys(pins[1])
 
         pin3 = driver.find_element(By.XPATH, '//input[@class="pincode-input-text"]/following::input/following::input')
-        pin3.send_keys(PIN3)
+        pin3.send_keys(pins[2])
 
         pin4 = driver.find_element(By.XPATH, '//input[@class="pincode-input-text"]/following::input/following::input/following::input')
-        pin4.send_keys(PIN4)
+        pin4.send_keys(pins[3])
     except TimeoutException:
-        print("Loading took too much time!")
+        print(user_email + ": input PIN failed!")
 
     time.sleep(3)
 
-def open_emiten_page(driver, emiten):
+def logout(user, driver):
+    print(user["email"] + ": logout")
+    driver.get(LOGOUT_URL)
+
+def open_emiten_page(user_email, driver, emiten):
     time.sleep(3)
+    print(user_email + ": open emiten page => " + emiten)
     driver.get(HOMEPAGE_URL + emiten)
+    time.sleep(3)
 
 def filter_non_digits(string: str) -> str:
     result = ''
@@ -79,84 +88,92 @@ def get_buying_power(driver):
     buy_power_num = 20_000_000
     return math.floor(buy_power_num / 4)
 
-def create_buy_order(driver, emiten, buy_price):
+def create_buy_order(user, driver, emiten, buy_price):
+    user_email = user["email"]
+
     # Open emiten page
-    open_emiten_page(driver, emiten)
+    open_emiten_page(user_email, driver, emiten)
 
     try:
-        WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.XPATH, '//*[@data-testid="btnBuy"]')))
-        print("Buy button OK")
+        WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.XPATH, '//button[@data-testid="btnBuy"]')))
+        print(user_email + ": button beli")
+
+        time.sleep(3)
 
         # Click Beli button
-        driver.find_element(By.XPATH, '//*[@data-testid="btnBuy"]').click()
-    except TimeoutException:
-        print("Buy botton does not exist!")
-
-    time.sleep(3)
-
-    # Input price
-    try:
-        WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.XPATH, '//input[@id="INPUT_BUY_PRICE"]')))
-        print("Input buy price OK")
-        input_price = driver.find_element(By.XPATH, '//input[@id="INPUT_BUY_PRICE"]')
-        input_price.send_keys(Keys.CONTROL + "A")
-        input_price.send_keys(Keys.DELETE)
-        input_price.send_keys(buy_price)
-    except TimeoutException:
-        print("Input buy price does not exist!")
-
-    time.sleep(3)
-
-    # Calculate position size (Lot)
-    buy_power = get_buying_power(driver)
-    buy_lot = buy_power / (int(buy_price) * 100)
-    input_lot = driver.find_element(By.XPATH, '//*[@id="INPUT_BUY_LOT"]')
-    input_lot.send_keys(Keys.CONTROL + "A")
-    input_lot.send_keys(Keys.DELETE)
-    input_lot.send_keys(math.floor(buy_lot))
-
-    time.sleep(3)
-
-    buy_btn = driver.find_element(By.XPATH, '//button[@data-testid="btnPopupBuy"]').is_enabled()
-    if buy_btn:
-        # Click Beli
-        driver.find_element(By.XPATH, '//button[@data-testid="btnPopupBuy"]').click()
-
-        # Konfirmasi Beli
+        driver.find_element(By.XPATH, '//button[@data-testid="btnBuy"]').click()
+        
+        # Input price
         try:
-            WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.XPATH, '//button[@data-testid="btnConfirmBuy"]')))
-            print("Konfirmasi beli OK")
-            driver.find_element(By.XPATH, '//button[@data-testid="btnConfirmBuy"]').click()
+            WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.XPATH, '//button[@data-testid="btnBuy"]')))
+            print(user_email + ": input harga")
+            input_price = driver.find_element(By.XPATH, '//input[@id="INPUT_BUY_PRICE"]')
+            input_price.send_keys(Keys.CONTROL + "A")
+            input_price.send_keys(Keys.DELETE)
+            input_price.send_keys(buy_price)
+
+            # Calculate position size (Lot)
+            buy_power = get_buying_power(driver)
+            buy_lot = buy_power / (int(buy_price) * 100)
+            input_lot = driver.find_element(By.XPATH, '//*[@id="INPUT_BUY_LOT"]')
+            input_lot.send_keys(Keys.CONTROL + "A")
+            input_lot.send_keys(Keys.DELETE)
+            input_lot.send_keys(math.floor(buy_lot))
+
+            print(user_email + ": buying power => " + str(buy_power))
+            print(user_email + ": buy lot => " + str(buy_lot))
+
+            buy_btn = driver.find_element(By.XPATH, '//button[@data-testid="btnPopupBuy"]').is_enabled()
+            if buy_btn:
+                # Click button Beli
+                driver.find_element(By.XPATH, '//button[@data-testid="btnPopupBuy"]').click()
+
+                # Click button Konfirmasi Beli
+                try:
+                    WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.XPATH, '//button[@data-testid="btnConfirmBuy"]')))
+                    print(user_email + ": konfirmasi beli")
+
+                    time.sleep(3)
+
+                    driver.find_element(By.XPATH, '//button[@data-testid="btnConfirmBuy"]').click()
+                    print(user_email + ": buying " + emiten + " SUCCESS!")
+                except TimeoutException:
+                    print(user_email + ": button konfirmasi beli does not exist!")
         except TimeoutException:
-            print("Element does not exist!")
-    print("Pembelian berhasil!")
+            print(user_email + ": input harga does not exist!")
+            create_buy_order(user, driver, emiten, buy_price)
+    except TimeoutException:
+        print(user_email + ": button beli does not exist!")
+        create_buy_order(user, driver, emiten, buy_price)
+
     time.sleep(3)
 
-def create_auto_order(driver, emiten, take_profit, cut_loss):
-    create_take_profit(driver, emiten, take_profit)
+def create_sell_order(user, driver, emiten, take_profit, cut_loss):
+    user_email = user["email"]
+    create_take_profit(user_email, driver, emiten, take_profit)
     time.sleep(5)
-    create_cut_loss(driver, emiten, cut_loss)
+    create_cut_loss(user_email, driver, emiten, cut_loss)
 
-def create_take_profit(driver, emiten, take_profit):
-    print('Start create take profit')
+def create_take_profit(user_email, driver, emiten, take_profit):
+    print(user_email + ": start create take profit")
 
     # Open emiten page
-    open_emiten_page(driver, emiten)
+    open_emiten_page(user_email, driver, emiten)
 
-    # Check Jual button
+    # Check jual button
     try:
-        WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.XPATH, '//*[@data-testid="btnSell"]')))
-        print("Button Jual OK")
+        WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.XPATH, '//button[@data-testid="btnSell"]')))
+        print(user_email + ": button jual")
 
-        time.sleep(1)
+        time.sleep(3)
 
         driver.find_element(By.XPATH, '//*[@data-testid="btnSell"]').click()
 
         # Select order type
-        select_auto_order_type(driver)
+        select_auto_order_type(user_email, driver)
 
         # Input expiry date
-        calculate_expiry_date(driver)
+        calculate_expiry_date(user_email, driver)
 
         # Click take profit
         driver.find_element(By.XPATH, '//div[@class="pl-4 pr-4 col-md-3"]').click()
@@ -182,30 +199,31 @@ def create_take_profit(driver, emiten, take_profit):
         sell_lot.send_keys(Keys.DELETE)
         sell_lot.send_keys(lot)
         
-        send_auto_order(driver)
+        send_auto_order(user_email, driver)
     except TimeoutException:
-        print("Button Jual does not exist!")
+        print(user_email + ": retrying button jual...")
+        create_take_profit(user_email, driver, emiten, take_profit)
 
-def create_cut_loss(driver, emiten, cut_loss):
-    print('Start create cut loss')
+def create_cut_loss(user_email, driver, emiten, cut_loss):
+    print(user_email + ": start create cut loss")
 
     # Open emiten page
-    open_emiten_page(driver, emiten)
+    open_emiten_page(user_email, driver, emiten)
 
-    # Check Jual button
+    # Check jual button
     try:
         WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.XPATH, '//*[@data-testid="btnSell"]')))
-        print("Button Jual OK")
+        print(user_email + ": button jual")
 
-        time.sleep(1)
+        time.sleep(3)
 
         driver.find_element(By.XPATH, '//*[@data-testid="btnSell"]').click()
 
         # Select auto order type
-        select_auto_order_type(driver)
+        select_auto_order_type(user_email, driver)
 
         # Input expiry date
-        calculate_expiry_date(driver)
+        calculate_expiry_date(user_email, driver)
 
         # Click cut loss
         driver.find_element(By.XPATH, '//div[@class="pl-0 pr-8 col-md-3"]').click()
@@ -231,21 +249,22 @@ def create_cut_loss(driver, emiten, cut_loss):
         sell_lot.send_keys(Keys.DELETE)
         sell_lot.send_keys(lot)
 
-        send_auto_order(driver)
+        send_auto_order(user_email, driver)
     except TimeoutException:
-        print("Button Jual does not exist!")
+        print(user_email + ": retrying button jual...")
+        create_cut_loss(user_email, driver, emiten, cut_loss)
 
-def select_auto_order_type(driver):
+def select_auto_order_type(user_email, driver):
     # Pick order type
     try:
         WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.XPATH, '//*[@id="periodType"]')))
-        print("Pilih Tipe Order OK")
+        print(user_email + ": pilih tipe order")
         driver.find_element(By.XPATH, '//*[@id="periodType"]').click()
         driver.find_element(By.XPATH, '//*[@data-testid="opt-periodType-1"]').click()
     except TimeoutException:
-        print("Element does not exist!")
+        print(user_email + ": pilih tipe order does not exist!")
 
-def calculate_expiry_date(driver):
+def calculate_expiry_date(user_email, driver):
     # Calculate expiry date
     start = date.today() + timedelta(days=1)
     end = start + timedelta(days=30)
@@ -261,7 +280,7 @@ def calculate_expiry_date(driver):
     # Input expiry start date
     try:
         WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.XPATH, '//div[@class="react-datepicker__input-container"]//input')))
-        print("Start date picker OK")
+        print(user_email + ": start date picker")
 
         # Click start date picker 
         driver.find_element(By.XPATH, '//div[@class="react-datepicker__input-container"]//input').click()
@@ -277,12 +296,12 @@ def calculate_expiry_date(driver):
         # Click start date
         driver.find_element(By.XPATH, "//div[text()='" + start_d + "'][@aria-disabled='false']").click()
     except TimeoutException:
-        print("Element does not exist!")
+        print(user_email + ": start date picker does not exist!")
 
     # Input expiry end date
     try:
         WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.XPATH, '//div[@class="react-datepicker__input-container"]//following::div[@class="react-datepicker__input-container"]')))
-        print("End date picker OK")
+        print(user_email + ": end date picker")
 
         # Click end date picker 
         driver.find_element(By.XPATH, '//div[@class="react-datepicker__input-container"]//following::div[@class="react-datepicker__input-container"]').click()
@@ -298,33 +317,35 @@ def calculate_expiry_date(driver):
         # Click end date
         driver.find_element(By.XPATH, "//div[text()='" + end_d + "'][@aria-disabled='false']").click()
     except TimeoutException:
-        print("Element does not exist!")
+        print(user_email + ": end date picker does not exist!")
 
-def send_auto_order(driver):
+def send_auto_order(user_email, driver):
     # Click aktifkan auto order
     try:
         WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.XPATH, '//*[@data-testid="btnPopupSell"]')))
-        print("Aktifkan auto order take profit OK")
+        print(user_email + ": aktifkan auto order take profit")
 
         time.sleep(3)
 
         driver.find_element(By.XPATH, '//*[@data-testid="btnPopupSell"]').click()
     except TimeoutException:
-        print("Element does not exist!")
+        print(user_email + ": aktifkan auto order take profit does not exist!")
 
     # Click konfirmasi auto order
     try:
         WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.XPATH, '//*[@data-testid="btnConfirmSell"]')))
-        print("Konfirmasi auto order take profit OK")
+        print(user_email + ": konfirmasi auto order take profit")
 
         time.sleep(3)
         
         driver.find_element(By.XPATH, '//*[@data-testid="btnConfirmSell"]').click()
     except TimeoutException:
-        print("Konfirmasi auto order take profit does not exist!")
+        print(user_email + ": konfirmasi auto order take profit does not exist!")
 
 # Delete chrome cache
-def delete_cache(driver):
+def delete_cache(user, driver):
+    print(user["email"] + ": delete cache!")
+
     driver.execute_script("window.open('')")  # Create a separate tab than the main one
     driver.switch_to.window(driver.window_handles[-1])  # Switch window to the second tab
     driver.get('chrome://settings/clearBrowserData')  # Open your chrome settings.
@@ -336,10 +357,7 @@ def delete_cache(driver):
 def perform_delete_cache(driver, keys):
     actions = ActionChains(driver)
     actions.send_keys(keys)
-
     time.sleep(2)
-
-    print('Performing delete cache!')
     actions.perform()
 
 def filter_non_digits(string: str) -> str:
